f08c8f3cf32782636f0f48886f9b8b27fd9a5836
Fitra Fadilana <fitra90@gmail.com>
2025-11-18 13:58:50 +0700

    add items to dropdown

--- routes/profile.js
+++ routes/profile.js
@@ -20,6 +20,12 @@ const profiles = [
   }
 ];
 
+const zodiac = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+
+const enneagram = ["1_wing_2", "2_wing_3", "3_wing_2", "3_wing_4", "4_wing_3", "4_wing_5", "5_wing_4", "5_wing_6", "6_wing_5", "6_wing_7", "7_wing_6", "7_wing_8", "8_wing_7", "8_wing_9", "9_wing_8", "9_wing_1"];
+
+const mbti = ["INFP", "INFJ", "ENFP", "ENFJ", "INTJ", "INTP", "ENTP", "ENTJ", "ISFP", "ISFJ", "ESFP", "ESFJ", "ISTP", "ISTJ", "ESTP", "ESTJ"];
+
 router.use(express.static('public'));
 
 module.exports = function() {
@@ -27,6 +33,9 @@ module.exports = function() {
   router.get('/*', function(req, res, next) {
     res.render('profile_template', {
       profile: profiles[0],
+      mbti: mbti,
+      enneagram: enneagram,
+      zodiac: zodiac,
     });
   });
 
